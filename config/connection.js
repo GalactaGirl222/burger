5b9065c1279bdd6733d9aaf4e883f09bb0db76ee
Simone <s.maya.cunningham@gmail.com>
2019-11-21 00:02:51 -0700

    updating server.js file and connections

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+var mysql = require("mysql");
+
+let connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'Password2020!',
+        database: 'burgers_db'
+    });
+}
+
+connection.connect(function (error) {
+    if (error) throw error;
+    console.log(`Connected to the database as ${connection.threadId}`);
+});
+
+module.exports = connection;
